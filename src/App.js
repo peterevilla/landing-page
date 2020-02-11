83b5cd6833227c58b0024325f3cc55b93fd80f4b
IT2 <it2@AWT.local>
2020-02-11 14:16:21 -0600

    testing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import Nav from './components/Nav'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+
+
+const App = () => {
+  
+  return (
+
+  <div>
+
+    <Nav/>
+    
+    <div className='header' ><Header /></div>
+    
+  </div>
+    
+
+  )
+  
+  
+}
 export default App;
